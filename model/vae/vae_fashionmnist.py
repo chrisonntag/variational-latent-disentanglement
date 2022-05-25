Finally fix backprop errors
I misused the __call__ method of keras.Model class, which should perform
a single forward pass (inputs -> outputs) but which I also used for
returning latent settings.

Z_mean and Z_log_var are now properties of the Model class which can be
called after a forward pass in order to calc reconstruction loss and KL
divergence.

model/vae/vae_fashionmnist.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
+import tensorflow_probability as tfp
 from tensorflow import keras
-from model.layers.sampling import Sampling
-from model.distributions import log_normal_pdf
+from model.layers import Sampling
 
 
 class Encoder(keras.Model):
@@ -34,9 +34,8 @@ class Encoder(keras.Model):
 
         # Sample latent vector Q(z|X) by splitting the last dense layer which is by design 2*z_dim
         z_mean, z_log_var = tf.split(x, num_or_size_splits=2, axis=1)
-        z = self.sampling([z_mean, z_log_var])
 
-        return z_mean, z_log_var, z
+        return z_mean, z_log_var
 
 
 class Decoder(keras.Model):
@@ -73,51 +72,43 @@ class Decoder(keras.Model):
 
 
 class VariationalAutoEncoderMNIST(keras.Model):
-    def __init__(self, input_dim=(28, 28, 1), z_dim=10, beta=2, prior=log_normal_pdf):
+    def __init__(self, input_dim=(28, 28, 1), z_dim=10, beta=2):
         super(VariationalAutoEncoderMNIST, self).__init__(name="FashionVAE")
+        self.z = None
+        self.z_log_var = None
+        self.z_mean = None
         self.input_dim = input_dim
         self.z_dim = z_dim
         self.beta = beta
-        self.prior = prior
         self.encoder = Encoder(input_dim, z_dim)
         self.decoder = Decoder(input_dim, z_dim)
 
-    def compute_loss(self, x, analytic=True):
-        """
-        Optimize the single sample Monte Carlo estimate of this expectation:
-        log p(x|z) + log p(z) - log q(z|x), where z is sampled from q(z|x)
-        """
-        z_mean, z_log_var, z = self.encoder(x)  # samples z based on distribution of x: q(z|x) posterior distribution
-        reconstruction = self.decoder(z)  # p(x|z)
+    def encode(self, x):
+        z_mean, z_log_var = self.encoder(x)
+        return z_mean, z_log_var
 
-        # Reconstruction loss
-        # shape=(batch_size, 28, 28, 1), cross entropy between p(x|z) and x for each dim of every datapoint
-        cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(logits=reconstruction, labels=x)
-        # shape=(batch_size,), cross entropy for all instances in batch x, logp(x|z)
-        logpx_z = -tf.reduce_mean(tf.reduce_sum(cross_entropy, axis=[1, 2, 3]))
+    def encode_dist(self, x):
+        z_mean, z_log_var = self.encoder(x)
+        return tfp.distributions.MultivariateNormalDiag(loc=z_mean, scale_diag=z_log_var)
 
-        # KL Divergence logp(z) - logq(z|x), if we assume q to be a multivariate Gaussian distribution
-        if analytic:
-            kl_divergence = - 0.5 * tf.reduce_sum(
-                1. + z_log_var - tf.square(z_mean) - tf.exp(z_log_var), axis=1)  # shape=(batch_size, )
-            kl_divergence = tf.reduce_mean(kl_divergence)
-        else:
-            # Monte Carlo estimation from a single sample
-            logpz = self.prior(z, 0., 1.)  # prior, approximated with sampled z
-            logqz_x = self.prior(z, z_mean, z_log_var)  # posterior p(z|x) approximated by learnt q(z|x)
+    def reparameterize(self, z_mean, z_log_var):
+        # samples z based on distribution (z_mean, z_log_var) of x: q(z|x) posterior distribution
+        batch = tf.shape(z_mean)[0]
+        dim = tf.shape(z_mean)[1]
 
-            kl_divergence = logpz - logqz_x
+        epsilon = keras.backend.random_normal(shape=(batch, dim))
+        return z_mean + tf.exp(0.5 * z_log_var) * epsilon
 
-        # -mean( logp(x|z) + logp(z) - logq(z|x) )
-        elbo = logpx_z + self.beta * kl_divergence
-        return -elbo, reconstruction
-
-    def call(self, x):
-        total_loss, reconstruction = self.compute_loss(x, analytic=True)
-        self.add_loss(total_loss)
-
+    def decode(self, z):
+        reconstruction = self.decoder(z)
         return reconstruction
 
+    def call(self, inputs):
+        self.z_mean, self.z_log_var = self.encode(inputs)
+        self.z = self.reparameterize(self.z_mean, self.z_log_var)
+        outputs = self.decode(self.z)
+
+        return outputs
 
 
 

train.py
@@ -62,8 +62,8 @@ for params in params_list:
     valid_ds = (tf.data.Dataset.from_tensor_slices(valid_images)).batch(params['batch_size'])
 
     # Create model and do Training
-    vae = VariationalAutoEncoderMNIST(z_dim=params['latent_dim'], beta=params['beta'], prior=log_normal_pdf)
-    trainer = Trainer(model=vae, params=params, optimizer=optimizer)
+    vae = VariationalAutoEncoderMNIST(z_dim=params['latent_dim'], beta=params['beta'])
+    trainer = Trainer(model=vae, params=params, optimizer=optimizer, prior=log_normal_pdf)
 
     train_history, val_history = trainer.train(train_ds, valid_ds)
     history = {"train_loss": train_history, "val_loss": val_history}

util/trainer.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 import numpy as np
 from tensorflow import keras
 import time
+from model.distributions import log_normal_pdf
+from model.layers import KullbackLeiblerDivergence, MSELoss
 
 
 class Stopper:
@@ -45,11 +47,13 @@ class Stopper:
 
 
 class Trainer:
-    def __init__(self, model, params, optimizer):
+    def __init__(self, model, params, optimizer, prior=log_normal_pdf):
         self.model = model
         self.params = params
         self.optimizer = optimizer
         self.train_stop = Stopper(optimizer)
+
+        self.prior = prior
 
         self.train_loss_tracker = keras.metrics.Mean(name="train_loss")
         self.val_loss_tracker = keras.metrics.Mean(name="val_loss")
@@ -61,11 +65,44 @@ class Trainer:
             self.val_loss_tracker,
         ]
 
+    def compute_loss(self, x, analytic=True, training=True):
+        """
+        Optimize the single sample Monte Carlo estimate of this expectation:
+        log p(x|z) + log p(z) - log q(z|x), where z is sampled from q(z|x)
+        """
+        z_mean, z_log_var, z, reconstruction = self.model(x, training=training)
+
+        # Reconstruction loss
+        # shape=(batch_size, 28, 28, 1), cross entropy between p(x|z) and x for each dim of every datapoint
+        cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(logits=reconstruction, labels=x)
+        # shape=(batch_size,), cross entropy for all instances in batch x, logp(x|z)
+        logpx_z = -tf.reduce_mean(tf.reduce_sum(cross_entropy, axis=[1, 2, 3]))
+
+        # KL Divergence logp(z) - logq(z|x), if we assume q to be a multivariate Gaussian distribution
+        if analytic:
+            kl_divergence = - 0.5 * tf.reduce_sum(
+                1. + z_log_var - tf.square(z_mean) - tf.exp(z_log_var), axis=1)  # shape=(batch_size, )
+            kl_divergence = tf.reduce_mean(kl_divergence)
+        else:
+            # Monte Carlo estimation from a single sample
+            logpz = self.prior(z, 0., 1.)  # prior, approximated with sampled z
+            logqz_x = self.prior(z, z_mean, z_log_var)  # posterior p(z|x) approximated by learnt q(z|x)
+
+            kl_divergence = logpz - logqz_x
+
+        # -mean( logp(x|z) + logp(z) - logq(z|x) )
+        elbo = logpx_z + self.model.beta * kl_divergence
+        return -elbo, reconstruction
+
     @tf.function
     def train_step(self, x_batch):
         with tf.GradientTape() as tape:
-            reconstruction = self.model(x_batch, training=True)
-            total_loss = sum(self.model.losses)
+            x_reconstruction = self.model(x_batch)
+
+            rec_los = tf.math.reduce_mean(MSELoss()(x_batch, x_reconstruction))
+            kl_loss = tf.math.reduce_mean(KullbackLeiblerDivergence()(self.model.z_mean, self.model.z_log_var))
+
+            total_loss = rec_los + self.model.beta * kl_loss
 
         grads = tape.gradient(total_loss, self.model.trainable_weights)
         self.optimizer.apply_gradients(zip(grads, self.model.trainable_weights))
@@ -74,8 +111,12 @@ class Trainer:
 
     @tf.function
     def val_step(self, x_batch):
-        reconstruction = self.model(x_batch, training=False)
-        total_loss = sum(self.model.losses)
+        x_reconstruction = self.model(x_batch)
+
+        rec_los = tf.math.reduce_mean(MSELoss()(x_batch, x_reconstruction))
+        kl_loss = tf.math.reduce_mean(KullbackLeiblerDivergence()(self.model.z_mean, self.model.z_log_var))
+
+        total_loss = rec_los + self.model.beta * kl_loss
         return total_loss
 
     def train(self, train_ds, val_ds):